388848c4cc5d3d9a80cd6fa96ff6346e1b2681aa
loren <blackcatoverwall@gmail.com>
2023-08-29 20:44:05 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(internal_features)]
 #![no_std]
 #![no_main]
 #![feature(lang_items)]
@@ -10,7 +11,7 @@ extern { }
 #[link(name = "ucrt")]
 extern { }
 
-use alloc::boxed::Box;
+use alloc::{boxed::Box, ffi::CString};
 
 #[no_mangle]
 static _fltused: i32 = 0;
@@ -27,8 +28,13 @@ pub fn personality() { }
 static GLOBAL_ALLOCATOR: Allocator = Allocator;
 
 #[no_mangle]
+#[allow(non_snake_case)]
 pub fn mainCRTStartup () -> isize {
     let _: Box<u8> = Box::new(0u8);
+    let msg = CString::new("Hello from mainCRTStartup").unwrap();
+    unsafe {
+        libc::printf(msg.as_c_str().as_ptr());
+    }
     0
 }
 
@@ -36,10 +42,10 @@ pub struct Allocator;
 
 unsafe impl alloc::alloc::GlobalAlloc for Allocator {
     unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
-        panic!("----------------")
+        libc::malloc(layout.size()) as *mut u8
     }
 
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {
-        todo!()
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: core::alloc::Layout) {
+        libc::free(ptr.cast())
     }
 }
