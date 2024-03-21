ba06d75362963f3ea06ae158c00892cf0148072b
araraloren <blackcatoverwall@gmail.com>
2024-03-21 23:03:05 +0800

    update bindgen example

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,35 @@
-use neure::{
-    ctx::{re_policy, PolicyCtx},
-    prelude::*,
+#[allow(non_upper_case_globals)]
+#[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
+#[allow(unused)]
+mod sqlite3 {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
+
+use std::{
+    ffi::{CStr, CString},
+    ptr::null_mut,
 };
 
-#[derive(Debug, Clone)]
-pub enum Ty<'a> {
-    Layer0(&'a str),
-
-    Layer1(&'a str, &'a str),
-
-    Layer2(&'a str, &'a str, &'a str),
-}
-
-#[derive(Debug, Clone)]
-pub struct Field<'a> {
-    #[allow(unused)]
-    ident: &'a str,
-
-    #[allow(unused)]
-    ty_name: Ty<'a>,
-
-    public: bool,
-}
-
-impl<'a> Field<'a> {
-    pub fn private(ident: &'a str, ty_name: Ty<'a>) -> Self {
-        Self {
-            ident,
-            ty_name,
-            public: false,
-        }
-    }
-
-    pub fn public(name: &'a str, ty_name: Ty<'a>) -> Self {
-        Self {
-            ident: name,
-            ty_name,
-            public: true,
-        }
-    }
-}
+use crate::sqlite3::sqlite3_errmsg;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let unit = neu::ascii_alphabetic()
-        .or(neu::ascii_alphanumeric())
-        .or('_');
-    let ident = unit.repeat_one_more();
-    let ty = neu::ascii_alphabetic()
-        .or('_')
-        .repeat_one()
-        .then(ident)
-        .pat();
-    // compile about 25s+
-    let layer1 = ty
-        .then(ty.quote("<", ">"))
-        .map(|(w, ty)| Ok(Ty::Layer1(w, ty)));
-    let layer2 = ty
-        .then(ty.then(ty.quote("<", ">")).quote("<", ">"))
-        .map(|(w1, (w2, ty))| Ok(Ty::Layer2(w1, w2, ty)));
-    let layer0 = ty.map(|ty| Ok(Ty::Layer0(ty)));
-    let field = ident.sep_once(":", layer2.or(layer1.or(layer0)));
-    // let layer1 = ty
-    //     .then(ty.quote("<", ">"))
-    //     .map(|(w, ty)| Ok(Ty::Layer1(w, ty)));
-    // let layer0 = ty.map(|ty| Ok(Ty::Layer0(ty)));
-    // let field = ident.sep_once(":", layer1.or(layer0));
-    // comment 55 ~ 62 and uncomment current block, compile about 2s+
-    let public = field
-        .padded("pub")
-        .map(|(name, ty_name)| Ok(Field::public(name, ty_name)));
-    let private = field.map(|(name, ty_name)| Ok(Field::private(name, ty_name)));
-    let parser = public.or(private).sep(",");
+    let mut db = null_mut();
+    let name = CString::new("test.db")?;
+    let rc = unsafe { sqlite3::sqlite3_open(name.as_ptr(), &mut db as *mut _) };
 
-    let data = "abc: Option<i32>";
-    let b_policy = re_policy(neu::whitespace().repeat_full());
+    if rc as u32 != sqlite3::SQLITE_OK {
+        let msg = unsafe { sqlite3_errmsg(db) };
+        panic!("Can not open database: {:?}", unsafe {
+            &CStr::from_ptr(msg)
+        });
+    } else {
+        println!("Open database successfully");
+    }
 
-    dbg!(PolicyCtx::new(CharsCtx::new(data), b_policy).ctor(&parser)?);
+    unsafe {
+        dbg!(sqlite3::sqlite3_close(db));
+    }
 
     Ok(())
 }
