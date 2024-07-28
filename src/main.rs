151bc59ec4749c5c2231bafda6d4c5e2d0064f73
araraloren <blackcatoverwall@gmail.com>
2024-07-28 11:45:06 +0800

    okay with reqwest websocket

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,6 @@ use futures_util::{SinkExt, TryStreamExt};
 use reqwest_websocket::RequestBuilderExt;
 use rustls::crypto::ring;
 use tokio_tungstenite::connect_async;
-use tokio_tungstenite::tungstenite::handshake::client::generate_key;
-use tokio_tungstenite::tungstenite::http::header::{
-    HOST, SEC_WEBSOCKET_KEY, SEC_WEBSOCKET_VERSION, UPGRADE,
-};
-use tokio_tungstenite::tungstenite::http::{Request, Uri};
 use tokio_tungstenite::tungstenite::Message;
 
 #[tokio::main]
@@ -30,19 +25,7 @@ async fn main() -> color_eyre::Result<()> {
 }
 
 pub async fn request(url: &str) -> color_eyre::Result<()> {
-    // Creates a GET request, upgrades and sends it.
-    let url = Uri::from_str(url)?;
-    let req = Request::get(&url)
-        .header("Connection", "Upgrade")
-        .header(HOST, url.host().unwrap())
-        .header(UPGRADE, "websocket")
-        .header(SEC_WEBSOCKET_VERSION, "13")
-        .header(SEC_WEBSOCKET_KEY, generate_key())
-        .body(())?;
-
-    println!("TOKIO: --> {req:?}");
-
-    let (ws_stream, _) = connect_async(req).await?;
+    let (ws_stream, _) = connect_async(url).await?;
 
     println!("TOKIO: -- connected!");
 
