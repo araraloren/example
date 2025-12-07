5fd4bf2b062f2cca865aac80b3b48a3a7eac341b
araraloren <blackcatoverwall@gmail.com>
2025-12-07 13:33:12 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use neure::prelude::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
     let array = regex::array(["a", "b", "c"]);
 
     // No match possible
     CharsCtx::with("xyz", move |mut ctx| {
-        assert!(array.parse(&mut ctx));
-        assert_eq!(ctx.offset(), 0);
+        array.try_parse(&mut ctx).unwrap();
     });
     Ok(())
 }
